53232d3a433e8d10790375a79732c8402001194e
OBir.Ninja <60829297+obirninja@users.noreply.github.com>
2020-03-16 04:18:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,5 @@ wget https://pastebin.obir.ninja/ts3proxy
 mkdir /usr/share/ts3proxy/
 chmod +x ts3proxy
 mv ts3proxy /usr/share/ts3proxy/ts3proxy
-wget https://gist.githubusercontent.com/obirninja/a15a801775e6b90bea97af0018982e41/raw/3ed1dccb3fa57faca4127ea04e545a50b884bbe3/ts3proxy
-chmod +x ts3proxy
-mv ts3proxy /usr/bin/ts3proxy
 clear
-echo "ts3proxy <- Yazarak yazilimi calistirabilirsiniz! Video:"
+echo "/usr/share/ts3proxy/ts3proxy <- Yazarak yazilimi calistirabilirsiniz! Video:"
