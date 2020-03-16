0bf2b2c04b43e7e2d8421f21ed9cc0f99d06c1d1
OBir.Ninja <60829297+obirninja@users.noreply.github.com>
2020-03-16 04:04:51 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Githubdaki kurulum adimlarina UYUNUZ!!"
+chmod +x install.sh
+wget https://pastebin.obir.ninja/ts3proxy
+chmod +x ts3proxy
+mv ts3proxy /usr/bin/ts3proxy
+clear
+echo "ts3proxy <- Yazarak yazilimi calistirabilirsiniz! Video:"
