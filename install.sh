2c1306ac43bb6e89dd0e4bfdf4f4d40ab1fa312a
OBir.Ninja <60829297+obirninja@users.noreply.github.com>
2020-03-16 04:12:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 echo "Githubdaki kurulum adimlarina UYUNUZ!!"
 chmod +x install.sh
 wget https://pastebin.obir.ninja/ts3proxy
+mkdir /usr/share/ts3proxy/
+chmod +x ts3proxy
+mv ts3proxy /usr/share/ts3proxy/ts3proxy
+echo 'bash /usr/share/ts3proxy/ts3proxy' > ts3proxy
 chmod +x ts3proxy
 mv ts3proxy /usr/bin/ts3proxy
 clear
