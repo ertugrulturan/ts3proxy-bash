de80803a34e9b6d3800225f26f0212e38f964b69
OBir.Ninja <60829297+obirninja@users.noreply.github.com>
2020-03-16 04:15:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget https://pastebin.obir.ninja/ts3proxy
 mkdir /usr/share/ts3proxy/
 chmod +x ts3proxy
 mv ts3proxy /usr/share/ts3proxy/ts3proxy
-echo 'bash /usr/share/ts3proxy/ts3proxy' > ts3proxy
+wget https://gist.githubusercontent.com/obirninja/a15a801775e6b90bea97af0018982e41/raw/3ed1dccb3fa57faca4127ea04e545a50b884bbe3/ts3proxy
 chmod +x ts3proxy
 mv ts3proxy /usr/bin/ts3proxy
 clear
